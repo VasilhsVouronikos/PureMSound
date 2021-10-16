1e168d23fea68f57dde00fb8fbed8dbbeef5a43b
VasilhsVouronikos <vasilhsvouronikos@gmail.com>
2021-10-17 00:49:07 +0300

    Update init_audio.py

--- init_audio.py
+++ init_audio.py
@@ -6,17 +6,29 @@ import struct
 
 CHUNK = 1024 * 2
 MODE = 'FX'
-WAV = 'acousticg.wav'
+WAV = 'samples\\guitar\\acousticg.wav'
 
+p = pa.PyAudio()
 
 def init():
-	p = pa.PyAudio()
 	if(MODE == 'FX'):
-		wf = wave.open('acousticg.wav', 'rb')
+		wf = wave.open(WAV, 'rb')
 		stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
 			channels=wf.getnchannels(),
 			rate=wf.getframerate(),
 			output=True)
 
+
+	latency = stream.get_output_latency()
+	input_device = list(p.get_default_input_device_info().values())[2]
+	output_device = list(p.get_default_output_device_info().values())[2]
+
+
+	get_sound_info(latency,input_device,output_device)
 	return p,wf,stream,CHUNK,WAV
+
+def get_sound_info(lat,inputd,output):
+	print("Output latency: ",lat,"\n",
+		"Input device(mic): ",inputd,"\n",
+		"Output device: ",output)
 
